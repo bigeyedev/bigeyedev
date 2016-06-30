65b6e5ceaf0a59d9e9894453655ade6d5fe58d46
bigeyedev <brstbf@gmail.com>
2016-06-30 14:44:49 +0700

    update

---
+++ bigeyedev/Controllers/CultureController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bigeyedev.Controllers
+{
+    public class CultureController : Controller
+    {
+        // GET: Culture
+        public ActionResult Index()
+        {
+            Session["Culture"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Thai()
+        {
+            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+            Session["Culture"] = "th-TH";
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+
+    }
+}
